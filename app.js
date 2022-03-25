ef0cb5fae92819124dd73cd8deb99a5ec0aab3ec
Roman Shaikh <roman@bit01.tech>
2022-03-25 18:35:21 +0530

    add views and webhook

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 global.express = require('express');
 const dotenv = require('dotenv');
-const { Shopify } = require('@shopify/shopify-api');
+const { Shopify, ApiVersion } = require('@shopify/shopify-api');
 const axios = require('axios');
 const path = require('path');
 var cors = require('cors');
@@ -10,73 +10,117 @@ const host = '127.0.0.1';
 const port = 3000;
 
 const app = express();
+app.set('view engine', 'ejs');
 dotenv.config();
 app.use(cors());
 require('./db/DB');
 const commonFunction = require('./helper/commonFunction.helper');
 const message = require('./helper/message');
-const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET, SHOPIFY_API_SCOPES, HOST } = process.env;
+const { SHOPIFY_API_KEY, SHOPIFY_API_SECRET_KEY, SHOPIFY_API_SCOPES, HOST } = process.env;
 
 const shops = {};
 
 Shopify.Context.initialize({
     API_KEY: SHOPIFY_API_KEY,
-    API_SECRET_KEY: SHOPIFY_API_SECRET,
-    SCOPES: SHOPIFY_API_SCOPES,
-    HOST_NAME: HOST,
-    IS_EMBEDDED_APP: true
+    API_SECRET_KEY: SHOPIFY_API_SECRET_KEY,
+    SCOPES: SHOPIFY_API_SCOPES.split(","),
+    HOST_NAME: HOST.replace(/https:\/\//, ""),
+    IS_EMBEDDED_APP: true,
+    API_VERSION: ApiVersion.April21,
 });
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 
 const AppSession = require('./models/appSession.model');
-const { Session } = require('@shopify/shopify-api/dist/auth/session');
-
 app.get('/', async (req, res) => {
     if (typeof shops[req.query.shop] !== 'undefined') {
+        // res.send('Welcome to shopify')
         res.sendFile(path.join(__dirname + '/index.html'));
-        // res.send('Hello world');
+        // res.render('home', { async: true, helper: commonFunction });
+        // res.render('home', { helper: await commonFunction.getProducts() })
     } else {
         res.redirect(`/auth?shop=${req.query.shop}`)
     }
 });
 
 app.get('/auth', async (req, res) => {
-    console.log('In auth/callback api')
-    const authRoute = await Shopify.Auth.beginAuth(
-        req,
-        res,
-        req.query.shop,
-        '/auth/callback',
-        false
-    )
-    res.redirect(authRoute);
+    console.log('In auth api')
+    try {
+        const authRoute = await Shopify.Auth.beginAuth(
+            req,
+            res,
+            req.query.shop,
+            '/auth/callback',
+            false
+        );
+        res.redirect(authRoute);
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
+    }
 });
 
 app.get('/auth/callback', async (req, res) => {
     console.log('In auth/callback api');
-    let shopSession = await Shopify.Auth.validateAuthCallback(
-        req,
-        res,
-        req.query
-    );
-    console.log('req.query : ', req.query);
-    console.log('shopSession : ', shopSession)
-    shops[shopSession.shop] = shopSession; // remove after store in db
-    let myShop = req.query.shop;
-    let storeSession = await AppSession.findOne({});
-    if (!storeSession) {
-        let newStore = new AppSession();
-        newStore.shop = shops[myShop].shop;
-        newStore.access_token = shops[myShop].accessToken;
-        await newStore.save();
+    try {
+        const shopifySession = await Shopify.Auth.validateAuthCallback(
+            req,
+            res,
+            req.query
+        );
+        console.log('shopifySession : ', shopifySession);
+        shops[shopifySession.shop] = shopifySession;
+        let myShop = req.query.shop;
+        let storeSession = await AppSession.findOne({});
+        if (!storeSession) {
+            let newStore = new AppSession();
+            newStore.shop = shops[myShop].shop;
+            newStore.access_token = shops[myShop].accessToken;
+            await newStore.save();
+        } else {
+            storeSession.access_token = shops[myShop].accessToken;
+            await storeSession.save();
+        }
+
+        // register webhook here
+        const response = await Shopify.Webhooks.Registry.register({
+            path: "/webhooks",
+            topic: "APP_UNINSTALLED",
+            accessToken: shopifySession.accessToken,
+            shop: shopifySession.shop,
+        });
+
+        console.log("resp at shopify callback ---------", response);
+        console.log(
+            "create response ---",
+            response["APP_UNINSTALLED"] ? response["APP_UNINSTALLED"].result : null
+        );
+
+        if (!response['APP_UNINSTALLED'].success) {
+            console.log(
+                `Failed to register APP_UNINSTALLED webhook: ${response.result}`
+            );
+        }
+        res.redirect(`https://${shops[myShop].shop}/admin/apps/`);
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
     }
-    storeSession.access_token = shops[myShop].accessToken;
-    await storeSession.save();
-    res.redirect(`https://${shops[myShop].shop}/admin/apps/`)
 });
 
 
-app.get('/app/products', async (req, res) => {
+app.get('/app/get-product-list', async (req, res) => {
     console.log('In app/product api');
     let url = 'https://' + req.query.shop + '/admin/products.json'
     console.log('url : ', url);
@@ -119,9 +163,7 @@ app.get('/app/products', async (req, res) => {
     }
 });
 
-app.post('/app/products/create', async (req, res) => {
-    // const client = new Shopify.Clients.Rest(shops[shopSession.shop], shops[shopSession.accessToken]);
-    // console.log(client)
+app.post('/app/create-product', async (req, res) => {
     console.log('in create product');
     let shopData = await commonFunction.getAppSession();
     console.log(shopData)
@@ -136,21 +178,158 @@ app.post('/app/products/create', async (req, res) => {
     let url = 'https://' + req.query.shop + '/admin/products.json'
     console.log('url : ', url);
     let data = {
-        "product": { "title": "Burton Custom Freestyle 151", "body_html": "\u003cstrong\u003eGood snowboard!\u003c\/strong\u003e", "vendor": "Burton", "product_type": "Snowboard", "tags": ["Barnes \u0026 Noble", "Big Air", "John's Fav"] }
+        "product": { "title": "Product 1", "body_html": "\u003cstrong\u003eGood snowboard!\u003c\/strong\u003e", "vendor": "Burton", "product_type": "Snowboard", "tags": ["Barnes \u0026 Noble", "Big Air", "John's Fav"] }
     }
     let headers = {
         'X-Shopify-Access-Token': shopData.access_token,
         'Content-Type': 'application/json'
     }
     try {
-        axios.post(url, data, { headers })
-            .then(response => console.log(response.data));
-
-        res.send(response.data);
+        let response = await axios.post(url, data, { headers });
+        console.log(response.data);
+        res.status(200).send({
+            status: constant.STATUS_CODE.SUCCESS,
+            message: message.GENERAL.data_save_success,
+            error: false,
+            data: response.data
+        });
     } catch (e) {
-        console.log('in error');
         console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
     }
+});
+
+app.get('/app/get-product/:id', async (req, res) => {
+    console.log('In get product detail api');
+    let url = 'https://' + req.query.shop + '/admin/api/2021-10/products/' + req.params.id + '.json';
+    console.log('url : ', url);
+    try {
+        let shopData = await commonFunction.getAppSession();
+        console.log(shopData)
+        if (!shopData) {
+            return res.status(400).send({
+                status: constant.STATUS_CODE.FAIL,
+                message: message.GENERAL.general_error_content,
+                error: true,
+                data: {}
+            });
+        }
+        let response = await axios.get(
+            url,
+            {
+                headers: {
+                    'X-Shopify-Access-Token': shopData.access_token,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        res.status(200).send({
+            status: constant.STATUS_CODE.SUCCESS,
+            message: message.GENERAL.data_fetch_success,
+            error: false,
+            data: response.data
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
+    }
+});
+
+app.post('/app/update-product/:id', async (req, res) => {
+    console.log('in update product');
+    let shopData = await commonFunction.getAppSession();
+    console.log(shopData)
+    if (!shopData) {
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
+    }
+    let url = 'https://' + req.query.shop + '/admin/api/2021-10/products/' + req.params.id + '.json'
+    console.log('url : ', url);
+    let data = {
+        "product": { "id": req.params.id, "title": " Update Burton Custom Freestyle 151", "body_html": "\u003cstrong\u003eGood snowboard!\u003c\/strong\u003e", "vendor": "Burton", "product_type": "Snowboard", "tags": ["Barnes \u0026 Noble", "Big Air", "John's Fav"] }
+    }
+    let headers = {
+        'X-Shopify-Access-Token': shopData.access_token,
+        'Content-Type': 'application/json'
+    }
+    try {
+        let response = await axios.put(url, data, { headers });
+        console.log(response.data);
+        res.status(200).send({
+            status: constant.STATUS_CODE.SUCCESS,
+            message: message.GENERAL.data_save_success,
+            error: false,
+            data: response.data
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
+    }
+});
+
+app.post('/app/delete-product/:id', async (req, res) => {
+    console.log('In delete product api');
+    let url = 'https://' + req.query.shop + '/admin/api/2021-10/products/' + req.params.id + '.json';
+    console.log('url : ', url);
+    try {
+        let shopData = await commonFunction.getAppSession();
+        console.log(shopData)
+        if (!shopData) {
+            return res.status(400).send({
+                status: constant.STATUS_CODE.FAIL,
+                message: message.GENERAL.general_error_content,
+                error: true,
+                data: {}
+            });
+        }
+        let response = await axios.delete(
+            url,
+            {
+                headers: {
+                    'X-Shopify-Access-Token': shopData.access_token,
+                    'Content-Type': 'application/json'
+                }
+            }
+        );
+        res.status(200).send({
+            status: constant.STATUS_CODE.SUCCESS,
+            message: message.GENERAL.data_fetch_success,
+            error: false,
+            data: response.data
+        });
+    } catch (e) {
+        console.log(e);
+        return res.status(400).send({
+            status: constant.STATUS_CODE.FAIL,
+            message: message.GENERAL.general_error_content,
+            error: true,
+            data: {}
+        });
+    }
+});
+
+app.post('/webhooks/app/uninstall', async (req, res) => {
+    console.log('In webhook');
+    console.log(req);
 });
 
 app.listen(port, () => {
--- helper/commonFunction.helper.js
+++ helper/commonFunction.helper.js
@@ -1,10 +1,28 @@
 const AppSession = require('../models/appSession.model');
+const axios = require('axios');
 
 exports.getAppSession = async () => {
     let shopSessionData = await AppSession.findOne({});
-    if(shopSessionData){
+    if (shopSessionData) {
         return shopSessionData
-    } else{
+    } else {
         return null
     }
+}
+
+exports.getProducts = async () => {
+    try {
+        console.log('In getProduct');
+        let url = 'https://https://20a7-103-249-233-8.ngrok.io/app/get-product-list?shop=first-step-demo.myshopify.com';
+        let response = await axios.get(
+            url,
+        );
+        console.log('products : ', response);
+        let products = response.data.data;
+        console.log(products)
+        return products
+    } catch (e) {
+        console.log(e);
+    }
+
 }
